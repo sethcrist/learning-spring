753e685b4b02228dbca4a0a7127bfecacdb48f97
Seth Crist <sethcrist50@gmail.com>
2024-07-03 19:39:01 -0500

    set up the data base for customers

---
+++ README.md
@@ -0,0 +1,4 @@
+## To see database
+- make sure docker is running
+- psql -U user (postgres_user in docker-compose.yml)
+- \l
--- src/main/java/com/learningspring/Main.java
+++ src/main/java/com/learningspring/Main.java
@@ -5,15 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-    @GetMapping("/greet")
-    public String greet() {
-        return "Hello World!";
     }
 }
